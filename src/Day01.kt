13c9291eee0ee9c75e1a783db32bb02339fe21d6
Paul Matthews <paul.matthews.86@gmail.com>
2023-12-03 21:34:20 +0000

    AOC Day 1
    * Parts 1 and 2
    * Semi Functional - requires restructuring

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val day = "Day01"
+    val numberWords: Map<String, String> = (1..9).associate { it.toString() to it.toString() } +
+            mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7,
+                "eight" to 8, "nine" to 9).mapValues { it.value.toString() }
+
+    fun List<String>.part1(): Int = sumOf {line ->
+        (line.first{it.isDigit()}.toString() + line.last{it.isDigit()}.toString()).toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun List<String>.part2(): Int = sumOf {line ->
+        val valueFirst = with(numberWords) {
+            numberWords.getOrDefault(
+                line.findAnyOf(keys, ignoreCase = true)?.second ?: "",
+                "0")
+        }
+        val valueLast = with(numberWords) {
+            numberWords.getOrDefault(
+                line.findLastAnyOf(keys, ignoreCase = true)?.second ?: "",
+                "0")
+        }
+        (valueFirst + valueLast).toInt()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testP1Input = readInput("${day}_test_p1")
+    val testP2Input = readInput("${day}_test_p2")
+    check(testP1Input.part1() == 142)
+    check(testP2Input.part2() == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("$day Part1: " + input.part1())
+    println("$day Part2: " + input.part2())
 }
