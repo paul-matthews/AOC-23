5d50b3a724037a967d223f934de40998facebf76
Paul Matthews <paul.matthews.86@gmail.com>
2023-12-13 22:10:55 +0000

    AOC Day 3 p2
    * Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,33 +2,34 @@ import Schematic.Companion.toSchematic
 
 fun Int.isWithin(start:Int, end:Int) = (start..end).contains(this)
 typealias Point = Pair<Int, Int>
-class Item(val start: Point, val end: Point, val value: String) {
-    fun hasAdjacentSymbol(symbols: List<Point>): Boolean =
-        symbols.any {
+class Item(private val start: Point, private val end: Point, val value: String) {
+    fun getMatchedSymbols(symbols: List<Item>): List<Item> =
+        symbols.filter {
             // Check there's a symbol on current, previous or next line
-            it.first.isWithin(start.first - 1, start.first + 1) &&
+            it.start.first.isWithin(start.first - 1, start.first + 1) &&
                     // That is next to the number
-                    it.second.isWithin(start.second - 1, end.second + 1)
+                    it.start.second.isWithin(start.second - 1, end.second + 1)
         }
 }
-data class Schematic(val items: List<Item>, val symbols: List<Point>) {
+data class Schematic(val items: List<Item>, val symbols: List<Item>) {
     companion object {
-        private val sym_regex = Regex("""(?<sym>[^\w.\n]{1})""")
+        private val sym_regex = Regex("""(?<sym>[^\w.\n])""")
         private val num_regex = Regex("""(?<num>\d+)""")
 
         fun Lines.toSchematic(): Schematic {
-            val symbols: MutableList<Point> = mutableListOf()
+            val symbols: MutableList<Item> = mutableListOf()
             // For Each Line
             return Schematic(flatMapIndexed {i, line ->
                 // Find all the symbols
                 sym_regex.findAll(line).forEach {match ->
                     match.groups["sym"]?.let {
                         // Store them
-                        symbols.add(Point(i, it.range.first))
+                        val pt = Point(i, it.range.first)
+                        symbols.add(Item(pt, pt, it.value))
                     }
                 }
                 // Find all the numbers
-                num_regex.findAll(line).fold(mutableListOf<Item>()) {items, match ->
+                num_regex.findAll(line).fold(mutableListOf()) {items, match ->
                     match.groups["num"]?.let {
                         // Store them
                         items.add(Item(Point(i, it.range.first), Point(i, it.range.last), it.value))
@@ -40,7 +41,21 @@ data class Schematic(val items: List<Item>, val symbols: List<Point>) {
     }
 
     fun getPartNumbers(): List<Item> =
-        items.filter { it.hasAdjacentSymbol(symbols) }
+        items.filter { it.getMatchedSymbols(symbols).isNotEmpty() }
+
+    fun getGearNumbers(): List<Pair<Item, Item>> {
+        val gearSymbols = symbols.filter { it.value == "*" }
+        val gearMatches: Map<Item, MutableList<Item>> = gearSymbols.associateWith { mutableListOf() }
+        items.forEach {item ->
+            item.getMatchedSymbols(gearSymbols).map {
+                gearMatches[it]?.add(item)
+            }
+        }
+        return gearMatches
+            .filter {(_, items) -> items.size == 2}
+            .flatMap { listOf(Pair(it.value.first(), it.value[1]))}
+
+    }
 }
 
 fun main() {
@@ -50,7 +65,10 @@ fun main() {
             .getPartNumbers()
             .sumOf { it.value.toInt() }
 
-//    fun Lines.part2() =
+    fun Lines.part2() =
+        toSchematic()
+            .getGearNumbers()
+            .sumOf { it.first.value.toInt() * it.second.value.toInt() }
 
     // test if implementation meets criteria from the description, like:
     readInput("${day}_test")
@@ -58,11 +76,14 @@ fun main() {
         .check(4361)
 
     // Calculate the real output
-    readInput(day)
+    val part1 = readInput(day)
         .part1()
+    part1
         .println("Part 1: ")
-//
-//    readInput(day)
-//        .part2()
-//        .println("Part 2: ")
+    part1
+        .check(540212)
+
+    readInput(day)
+        .part2()
+        .println("Part 2: ")
 }
